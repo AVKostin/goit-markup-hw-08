f3f814249f081cb76910682291114ab73b55e9c5
AVKostin <alexander.kostin.ua@gmail.com>
2021-11-09 14:21:18 +0200

    fix bugs

--- js/menu.js
+++ js/menu.js
@@ -20,3 +20,25 @@
   closeMenuBtn.addEventListener('click', toggleMenu);
 
 })();
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('.menu-open-btn'),
+//     closeMenuBtn: document.querySelector('[.menu-close-btn]'),
+//     menu: document.querySelector('.mob-menu'),
+//     menuItem: document.querySelector('.mob-menu')
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+//   refs.menu.addEventListener('click', removeMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//   }
+//   function removeMenu(e) {
+//     if (e.target === refs.menu) {
+//       refs.menu.classList.add('is-hidden');
+//     }
+//   }
+// })();
